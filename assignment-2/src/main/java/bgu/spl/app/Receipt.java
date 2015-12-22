829e3f1e43a06d2a37a5ef59c84862598d3e829c
Yoav Beeri <ireeeb@gmail.com>
2015-12-22 20:27:47 +0200

    add getters

--- assignment-2/src/main/java/bgu/spl/app/Receipt.java
+++ assignment-2/src/main/java/bgu/spl/app/Receipt.java
@@ -6,8 +6,16 @@ public class Receipt {
 	private String customer;
 	private String shoeType;
 	private boolean discount;
-	public int issuedTick;
-	private int requestTic;
+	private int issuedTick;
+	private int requestTick;
 	private int amountSold;
+	
+	public String getSeller(){return seller;}
+	public String getCustomer(){return customer;}
+	public String getType(){return shoeType;}
+	public boolean isDiscounted(){return discount;}
+	public int getIssuedTick(){return issuedTick;}
+	public int getRequestTick(){return requestTick;}
+	public int getAmountSold(){return amountSold;}
 
 }
