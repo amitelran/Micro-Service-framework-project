0b6ef6bfd455e4c2d5f6775dc2eefc9c5abd5e14
Yoav Beeri <ireeeb@gmail.com>
2015-12-23 23:46:01 +0200

    ManagementService

---
+++ assignment-2/src/main/java/bgu/spl/app/ManagementService.java
@@ -0,0 +1,91 @@
+package bgu.spl.app;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import bgu.spl.mics.MicroService;
+
+public class ManagementService extends MicroService {
+	
+	private class orderedAndReserved{
+		int ordered;
+		int reserved;
+		ConcurrentLinkedQueue<RestockRequest> requests;
+		
+		public orderedAndReserved(int ordered,int reserved,RestockRequest req){
+			this.ordered=ordered;
+			this.reserved=reserved;
+			requests.add(req);
+		}
+	}
+	private List<DiscountSchedule> DiscountSchedule;
+	private int currentTick;
+	private Map<String,orderedAndReserved> orders;
+	
+	public ManagementService(List<DiscountSchedule> DiscountSchedule) {
+		super("manager");
+		this.DiscountSchedule=DiscountSchedule;
+		currentTick=1;
+		orders = new ConcurrentHashMap<String,orderedAndReserved>();
+	}
+
+	@Override
+	protected void initialize() {
+		this.subscribeBroadcast(TickBroadcast.class, b->{
+			currentTick=b.getTick();
+			for(DiscountSchedule dS : DiscountSchedule){
+				if(dS.getTick()>=currentTick){
+					DiscountSchedule.remove(dS);
+					this.sendBroadcast(new NewDiscountBroadcast(dS.getShoeType(), dS.getAmount()));
+				}
+			}
+		});
+		
+		this.subscribeRequest(RestockRequest.class, req -> {
+			synchronized(orders){
+				String shoe=req.getShoeType();
+				int amount=req.getAmount();
+				orderedAndReserved order = orders.get(shoe);
+				if( order==null || order.ordered-order.reserved<amount ){
+					int newOrderAmount=currentTick%5+1;
+					if(order==null){
+						order=new orderedAndReserved(newOrderAmount,1,req);
+						orders.put(shoe, order);
+					}
+					else{
+						order.ordered+=newOrderAmount;
+						order.reserved++;
+						order.requests.add(req);
+					}
+					this.sendRequest(new ManufacturingOrderRequest(shoe, newOrderAmount), res->{
+						synchronized(orders){
+							
+							if(res!=null){
+								if(res.getAmountSold()-orders.get(shoe).reserved>0)
+									Store.getInstance().add(shoe, res.getAmountSold()-orders.get(shoe).reserved);
+								
+								for(int i=0;i<res.getAmountSold()&&!orders.get(shoe).requests.isEmpty();i++)
+									this.complete(orders.get(shoe).requests.poll(), true);
+								
+								if(orders.get(shoe).requests.isEmpty())orders.remove(shoe);
+							}
+							else{
+								for(RestockRequest rr : orders.get(shoe).requests){
+									this.complete(rr, false);
+								}
+								orders.remove(shoe);
+							}
+						}
+					});
+				}
+				else{
+					order.requests.add(req);
+				}
+			}
+		});
+		
+	}
+
+}
