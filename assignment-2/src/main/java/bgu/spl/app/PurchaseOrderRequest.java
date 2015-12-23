f857d32ca84178fbac4f2c4eb5a49ced52a0da25
Yoav Beeri <ireeeb@gmail.com>
2015-12-23 21:48:16 +0200

    sender name

--- assignment-2/src/main/java/bgu/spl/app/PurchaseOrderRequest.java
+++ assignment-2/src/main/java/bgu/spl/app/PurchaseOrderRequest.java
@@ -7,16 +7,19 @@ public class PurchaseOrderRequest implements Request<Receipt> {
 	private int amount;
 	private boolean onlyOnDiscount;
 	private int tick;
+	private String senderNane;
 	
-	public PurchaseOrderRequest(String shoeType,int amount,boolean onlyOnDiscount,int tick){
+	public PurchaseOrderRequest(String shoeType,int amount,boolean onlyOnDiscount,String senderNane,int tick){
 		this.shoeType=shoeType;
 		this.amount=amount;
 		this.onlyOnDiscount=onlyOnDiscount;
+		this.senderNane=senderNane;
 		this.tick=tick;
 	}
 	
 	public String getType(){return shoeType;}
 	public int getAmount(){return amount;}
 	public boolean onlyOnDiscount(){return onlyOnDiscount;}
+	public String getSenderName(){return senderNane;}
 	public int getRequestedTime(){return tick;}
 }
