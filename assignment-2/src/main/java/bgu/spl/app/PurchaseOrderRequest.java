3380fa7a0900484832191f5fc98aaac5a6bc7af8
Yoav Beeri <ireeeb@gmail.com>
2015-12-23 21:16:12 +0200

    purchaseorderrequest add time (tick)

--- assignment-2/src/main/java/bgu/spl/app/PurchaseOrderRequest.java
+++ assignment-2/src/main/java/bgu/spl/app/PurchaseOrderRequest.java
@@ -6,14 +6,17 @@ public class PurchaseOrderRequest implements Request<Receipt> {
 	private String shoeType;
 	private int amount;
 	private boolean onlyOnDiscount;
+	private int tick;
 	
-	public PurchaseOrderRequest(String shoeType,int amount,boolean onlyOnDiscount){
+	public PurchaseOrderRequest(String shoeType,int amount,boolean onlyOnDiscount,int tick){
 		this.shoeType=shoeType;
 		this.amount=amount;
 		this.onlyOnDiscount=onlyOnDiscount;
+		this.tick=tick;
 	}
 	
 	public String getType(){return shoeType;}
 	public int getAmount(){return amount;}
 	public boolean onlyOnDiscount(){return onlyOnDiscount;}
+	public int getRequestedTime(){return tick;}
 }
