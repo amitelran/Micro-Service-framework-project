790f0f6f1e12112d78b3c687f3e68e70853c37d7
amitelran <amitelran2110@gmail.com>
2015-12-23 23:31:22 +0200

    Selling Service

--- assignment-2/src/main/java/bgu/spl/app/SellingService.java
+++ assignment-2/src/main/java/bgu/spl/app/SellingService.java
@@ -2,45 +2,66 @@ package bgu.spl.app;
 
 import bgu.spl.app.Store.BuyResult;
 import bgu.spl.mics.MicroService;
-import bgu.spl.mics.example.messages.ExampleRequest;
-import bgu.spl.mics.impl.MessageBusImpl;
+
 
 public class SellingService extends MicroService {
+	
+	private int currentTick;
 
 	public SellingService(String name){
 		super(name);
+		currentTick=1;
 		
 	}
 
 	@Override
 	protected void initialize() {
 		System.out.println("Selling Service " + getName() + " started");
+		this.subscribeBroadcast(TickBroadcast.class, b->{
+			currentTick=b.getTick();
+		});
 		subscribeRequest(PurchaseOrderRequest.class, purReq -> {
-			BuyResult result = Store.getInstance().take(purReq.getType(), purReq.onlyOnDiscount());
+			BuyResult result;
+			try {
+				result = Store.getInstance().take(purReq.getType(), purReq.onlyOnDiscount());
 			if (result == BuyResult.Regular_Price){
-				Receipt rec = new Receipt(this.getName(), name, name, terminated, 0, 0, 0)
+				Receipt rec = new Receipt(this.getName(), purReq.getSenderName(), purReq.getType(), false, this.currentTick, purReq.getRequestedTime(), 1);
+				Store.getInstance().file(rec);
+				this.complete(purReq,rec);
 			}
 			else {
 				if (result == BuyResult.Discounted_Price){
-					//
+					Receipt rec = new Receipt(this.getName(), purReq.getSenderName(), purReq.getType(), true, this.currentTick, purReq.getRequestedTime(), 1);
+					Store.getInstance().file(rec);
+					this.complete(purReq,rec);
 				}
 				else{
 					if (result == BuyResult.Not_On_Discount){
-						
+						this.complete(purReq,null);
 					}
 					else{
 						if (result == BuyResult.Not_In_Stock){
 							
+							this.sendRequest(new RestockRequest(purReq.getType(),1), ans -> {
+								if (ans==false){
+									this.complete(purReq, null);
+								}
+								else{
+									Receipt rec = new Receipt(this.getName(), purReq.getSenderName(), purReq.getType(), false, this.currentTick, purReq.getRequestedTime(), 1);
+									Store.getInstance().file(rec);
+									this.complete(purReq,rec);
+								}
+							});
 						}
 					}
 				}
 			
            
             }
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
         });
-		
 	}
-
-	
-	
 }
