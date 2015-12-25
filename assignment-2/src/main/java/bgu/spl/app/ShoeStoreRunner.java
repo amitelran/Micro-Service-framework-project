b59ad50691b24156c809da16bd3dcd9374978519
amitelran <amitelran2110@gmail.com>
2015-12-25 12:49:23 +0200

    pom.xml GSON minor changes

---
+++ assignment-2/src/main/java/bgu/spl/app/ShoeStoreRunner.java
@@ -0,0 +1,14 @@
+package bgu.spl.app;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import com.google.code.gson;
+
+public class ShoeStoreRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
