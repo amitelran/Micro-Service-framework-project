76093ddb4ba339db7162e50a634fee663ad24498
amitelran <amitelran2110@gmail.com>
2015-12-29 17:46:27 +0200

    updates:
    getShoeInfo method in Store class used as helper method for JUnit
    testing
    getReceipt method in Store class used as helper method for JUnit testing
    getStorageInfo method in Store class used as helper method for JUnit
    testing

--- assignment-2/src/test/java/bgu/spl/mics/impl/MessageBusImplTest.java
+++ assignment-2/src/test/java/bgu/spl/mics/impl/MessageBusImplTest.java
@@ -25,18 +25,6 @@ public class MessageBusImplTest {
 	@Before
 	public void setUp() throws Exception {
 		messageBus = MessageBusImpl.getInstance();
-		List<DiscountSchedule> discountSchedule=new LinkedList<DiscountSchedule>();
-		discountSchedule.add(new DiscountSchedule("green-flip-flops",10,3));
-		ManagementService management=new ManagementService(discountSchedule,null);		//management can sell 3 discounted green-flip-flops from tick 10 
-		Set<String> BRURIAs=new HashSet<String>();
-		BRURIAs.add("green-flip-flops");
-		List<PurchaseSchedule> psBRURIA=new LinkedList<PurchaseSchedule>();
-		psBRURIA.add(new PurchaseSchedule("red-boots", 3));	
-		WebsiteClientService cS1=new WebsiteClientService("Bruria", psBRURIA,BRURIAs,null);		//Bruria would want to purchase red-boots from tick 3
-		Set<String> SHRAGAs=new HashSet<String>();
-		List<PurchaseSchedule> psSHRAGA=new LinkedList<PurchaseSchedule>();
-		psSHRAGA.add(new PurchaseSchedule("green-flip-flops", 12));
-		WebsiteClientService cS2=new WebsiteClientService("Shraga", psSHRAGA,SHRAGAs ,null);	//Shraga would want to purchase green-flip-flops from tick 12
 	}
 
 	@After
